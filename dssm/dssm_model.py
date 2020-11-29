07fd53355423b432f3c4a06bf51b4cb47756f870
michael.liu <841412988@qq.com>
2020-11-29 15:55:08 +0800

    new project

---
+++ dssm/dssm_model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/11/29 3:52 PM
+# @Author  : Michael.liu
+# @Site    : 
+# @File    : dssm_model.py
+# @Software: PyCharm Community Edition
+
+
+
+
+
+
+
+if __name__=='__main__':
+    print("hello world!")
